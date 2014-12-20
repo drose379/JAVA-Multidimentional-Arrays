8b72b20ebcb76764acc50f890b949be96abcbe3c
drose379 <dylanrose60@DylanRosMacBook.home>
2014-12-20 14:20:28 -0500

    Learning Java multidimensional arrays

---
+++ MultiDimentionalArrays.java
@@ -0,0 +1,41 @@
+
+
+package multidimentional.arrays;
+
+public class MultiDimentionalArrays {
+
+    public static void main(String[] args) {
+       int firstArray[][]= {{1,2,4},
+                            {4,5,3},
+                            {3,4,6},
+                            {7,2,1,6}
+                            };
+
+       System.out.println("The 'array length' is: "+firstArray.length);
+       System.out.println("Testing row length: "+ firstArray[0].length);
+       /*How the create method works*/
+       System.out.print(firstArray[0][0]+"\t");
+       System.out.print(firstArray[0][1]+"\t");
+       System.out.print(firstArray[0][2]+"\t");
+       //Next row
+       
+       //create(firstArray);
+    }
+    
+    public static void buildTable(int Array[][]) {
+        for (int row=0;row < Array.length;row++) {
+            
+        }
+    }
+    
+    /*public static void create(int Multiarray[][]) {
+        for (int row=0;row < Multiarray.length;row++) {
+            for (int column=0;column<Multiarray[row].length;column++) {
+                System.out.print(Multiarray[row][column]+"\t");
+            }
+            System.out.println();
+    }*/
+
+}
+    
+
